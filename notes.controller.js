93b9d7b2c6b83837f04a3d517e5b2ed847591ba1
Anton Krat <threesundaysonewerk@gmail.com>
2023-09-28 11:24:25 +0800

    initial project

---
+++ notes.controller.js
@@ -0,0 +1,44 @@
+const fs = require("fs/promises");
+const path = require("path");
+const chalk = require("chalk");
+
+const notesPath = path.join(__dirname, "db.json");
+
+async function addNote(title) {
+  const notes = await getNotes();
+
+  const note = {
+    title,
+    id: Date.now().toString(),
+  };
+
+  notes.push(note);
+
+  await fs.writeFile(notesPath, JSON.stringify(notes));
+}
+
+async function getNotes() {
+  const notes = await fs.readFile(notesPath, { encoding: "utf8" });
+  return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : [];
+}
+
+async function printNotes() {
+  const notes = await getNotes();
+
+  console.log(chalk.yellow("Here the list of notes"));
+  notes.forEach((note) => {
+    console.log(chalk.blue(note.id, note.title));
+  });
+}
+
+async function removeNote(id) {
+  const notes = await getNotes();
+  const filteredNotes = notes.filter((note) => note.id !== id);
+  await fs.writeFile(notesPath, JSON.stringify(filteredNotes));
+}
+
+module.exports = {
+  addNote,
+  removeNote,
+  printNotes,
+};
